d8c131b8a4a11e16e8cf966bbfd6b56f6a485985
Emmanuel <emmannuell.sanchhez@gmail.com>
2024-10-23 14:33:29 -0600

    Clase GestorMantenimiento con singleton

--- Model/GestorMantenimiento.java
+++ Model/GestorMantenimiento.java
@@ -4,12 +4,48 @@
  */
 package Model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Student
  */
 public class GestorMantenimiento {
     
+    private ArrayList<vehiculo> vehiculos; 
+    
+    private GestorMantenimiento instance;
+
+    private GestorMantenimiento() {
+        try{
+            Thread.sleep(1000);
+        }catch(InterruptedException ex){
+            ex.printStackTrace();
+        }
+        this.vehiculos = new ArrayList<>();
+    }
+    
+    public synchronized GestorMantenimiento getInstance(){
+        if(instance == null){
+            instance = new GestorMantenimiento();
+        }
+        return instance;
+        
+    }
+    
+    public static void Agregar(vehiculo vehiculo){
+        vehiculos.add(vehiculo)
+    }
+    public static void Eliminar(vehiculo vehiculo){
+        vehiculos.remove(vehiculo);
+    }
+    public static void Buscar(int id){
+        for(vehiculo v1 : vehiculos){
+            if(v1.cliente.getId == vehiculo.cliente.getId){
+                return v1;
+            }
+        }
+    }
     
     
 }

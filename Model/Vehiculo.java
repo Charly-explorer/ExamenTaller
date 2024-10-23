75e5fed732b587d023ee08b68c00e65c9e85fefa
Zuly <zurojasco@est.utn.ac.cr>
2024-10-23 14:27:54 -0600

    Clase Vehicul 2.o
    Creacion de clase vehiculo con sus metodos y atributos

--- Model/Vehiculo.java
+++ Model/Vehiculo.java
@@ -9,5 +9,53 @@ package Model;
  * @author Student
  */
 public class Vehiculo {
-    
+  private String marca;
+  private String modelo; 
+  private String matricula; 
+  private Cliente cliente; 
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public Vehiculo(String marca, String modelo, String matricula, Cliente cliente) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.matricula = matricula;
+        this.cliente = cliente;
+    }
+  
+  public Vehiculo (){
+      this("","","",null);
+  }
+
+    @Override
+    public String toString() {
+        return "Vehiculo{" + "marca=" + marca + ", modelo=" + modelo + ", matricula=" + matricula + ", cliente=" + cliente + '}';
+    }
+  
 }

63a3caeedf1a90ce76a9659afc22f43fec3a891c
Emmanuel <emmannuell.sanchhez@gmail.com>
2024-10-23 15:19:50 -0600

    Modificacion de GestorMantenimineto ahora almacena servicios y agregar metodo contador

--- Model/GestorMantenimiento.java
+++ Model/GestorMantenimiento.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
  */
 public class GestorMantenimiento {
 
-    private ArrayList<Vehiculo> vehiculos;
+    private ArrayList<ServicioMantenimiento> servicios;
 
     private GestorMantenimiento instance;
 
@@ -22,7 +22,7 @@ public class GestorMantenimiento {
         } catch (InterruptedException ex) {
             ex.printStackTrace();
         }
-        this.vehiculos = new ArrayList<>();
+        this.servicios = new ArrayList<>();
     }
 
     public synchronized GestorMantenimiento getInstance() {
@@ -31,22 +31,30 @@ public class GestorMantenimiento {
         }
         return instance;
     }
-
-    public void Agregar(Vehiculo vehiculo) {
-        vehiculos.add(vehiculo);
+    
+    public void Agregar(ServicioMantenimiento servicio) {
+        servicios.add(servicio);
     }
 
-    public void Eliminar(Vehiculo vehiculo) {
-        vehiculos.remove(vehiculo);
+    public void Eliminar(ServicioMantenimiento servicio) {
+        servicios.remove(servicio);
     }
 
-    public Vehiculo Buscar(String id) {
-        for (Vehiculo v1 : vehiculos) {
-            if (v1.getCliente().getId() == id) {
+    public Vehiculo BuscarVehiculoCliente(String id) {
+        for (ServicioMantenimiento v1 : servicios) {
+            if (v1.getVehiculo().getCliente().getId == id){
                 return v1;
             }
         }
         return null;
+    }
+    
+    public int ContadorServicios(){
+        int cont = 0;
+        for (ServicioMantenimiento v1 : servicios){
+            cont =+ 1;
+        }
+        return cont;
     }
     
 }
